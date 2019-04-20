0ec991b81ebab761cc104bd783bc04f58dc827dd
Qson8 <455063392@qq.com>
2019-04-20 10:59:13 +0800

    搭建原生界面

---
+++ NativeFlutterDemo/Demo/AppDelegate.m
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.m
+//  NativeFlutterDemo
+//
+//  Created by Qson on 2019/4/20.
+//  Copyright © 2019 Qson. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    ViewController *vc = [[ViewController alloc] init];
+    UINavigationController *nvc = [[UINavigationController alloc] initWithRootViewController:vc];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+@end
---
+++ NativeFlutterDemo/Demo/ViewController.m
@@ -0,0 +1,65 @@
+//
+//  ViewController.m
+//  NativeFlutterDemo
+//
+//  Created by Qson on 2019/4/20.
+//  Copyright © 2019 Qson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tableView;
+@end
+
+@implementation ViewController
+
+- (UITableView *)tableView {
+    if(_tableView == nil) {
+        UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectZero style:(UITableViewStylePlain)];
+        tableView.delegate = self;
+        tableView.dataSource = self;
+        [self.view addSubview:tableView];
+        _tableView = tableView;
+    }
+    return _tableView;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self tableView];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    self.tableView.frame = self.view.bounds;
+}
+
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *txt = [NSString stringWithFormat:@"%ld 点击进入flutter世界",indexPath.row];
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:(UITableViewCellStyleSubtitle) reuseIdentifier:@"cell"];
+    cell.textLabel.text = txt;
+    return cell;
+}
+
+#pragma mark -UITableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+@end
